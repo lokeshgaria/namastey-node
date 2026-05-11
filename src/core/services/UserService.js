192f4a2131617d77f261f7f2107db4f7a294ea6e
lokeshgaria <lokeshgaria8811@gmail.com>
2026-05-11 17:25:07 +0530

    fix(cache): add validate method on update profile

--- src/core/services/UserService.js
+++ src/core/services/UserService.js
@@ -1,7 +1,7 @@
 class UserService {
   constructor(userRepository, cacheService) {
-    this.userRepo = userRepository;
-    this.cache = cacheService;
+    this.userRepo = userRepository
+    this.cache = cacheService
   }
 
   // getUser = async (req) => {
@@ -17,50 +17,54 @@ class UserService {
   //   }
   // };
 
-    /**
+  /**
    * Get user profile (with caching)
    */
-    getUser = async (req) => {
-      const userId = req.user._id;
-      return await this.cache.getUserProfile(
-        userId,
-        async () => {
-          // Fetch from database if not in cache
-          const user = await this.userRepo.findByIdSafe(userId);
-          if (!user) {
-            throw new Error('User not found');
-          }
-          return user.toObject();
-        }
-      );
-    }
+  getUser = async (req) => {
+    const userId = req.user._id
+    return await this.cache.getUserProfile(userId, async () => {
+      // Fetch from database if not in cache
+      const user = await this.userRepo.findByIdSafe(userId)
+      if (!user) {
+        throw new Error('User not found')
+      }
+      return user.toObject()
+    })
+  }
 
-    
+  // updateProfile = async (req) => {
+  //   try {
+  //     const userId = req.user._id;
+  //     const updateData = req.body;
+  //     const user = await this.userRepo.updateById(userId, updateData, { new: true });
+  //     if (!user) {
+  //       throw new Error("User not found");
+  //     }
+  //     return user;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
 
   updateProfile = async (req) => {
-    try {
-      const userId = req.user._id;
-      const updateData = req.body;
-      const user = await this.userRepo.updateById(userId, updateData, { new: true });
-      if (!user) {
-        throw new Error("User not found");
-      }
-      return user;
-    } catch (error) {
-      throw error;
+    const userId = req.user._id
+    const updateData = req.body
+    const user = await this.userRepo.updateById(userId, updateData, { new: true })
+    if (!user) {
+      throw new Error('User not found')
     }
-  };
+
+    // Invalidate old cached profile so next getUser fetches fresh data
+    await this.cache.invalidateUserProfile(userId)
+
+    return user
+  }
 
   getFeed = async (req) => {
-    try {
-      const userId = req.user._id;
-      const feed = await this.userRepo.findExcludingIds(userId);
-      return feed;
-    } catch (error) {
-      throw error;
-    }
-  };
- 
+    const userId = req.user._id
+    const feed = await this.userRepo.findExcludingIds(userId)
+    return feed
+  }
 }
 
-module.exports = UserService;
+module.exports = UserService
