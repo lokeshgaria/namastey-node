1267d4dbf53236232273398397d406c05e720bf8
lokesh <63031525+lokeshgaria@users.noreply.github.com>
2026-08-21 16:16:22 +0530

    Update AuthController.js

--- src/api/controllers/AuthController.js
+++ src/api/controllers/AuthController.js
@@ -14,7 +14,7 @@ class AuthController {
 
       // set http-only cookie
       res.cookie('token', await user.getJWT(), {
-        httpOnly: process.env.NODE_ENV === 'production' ? false:true,
+        httpOnly:false,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
